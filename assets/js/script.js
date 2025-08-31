badf111c2817a8d34b9f6b0adf71f06a117b99cb
Andres <andydquintero@gmail.com>
2025-08-30 21:49:01 -0500

    ScriptDeletesAndKeepsTrackOfCompletedTasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+const container = document.querySelector(".container");
+
 const verificar = document.querySelector("button");
 
 const input = document.querySelector("input");
@@ -10,11 +12,20 @@ const stats = document.querySelector(".stats");
 
 const total = document.querySelector("#total");
 
-const container = document.querySelector(".status");
+const statusBoxes = document.querySelector(".status");
+
+const result = document.querySelector("#resultado");
 
 var counter = 0;
+var counterResult = 0;
 function getRandomInRange(min, max) {
   return Math.random() * (max - min) + min;
+}
+function adjustContainerHeight() {
+  const baseHeight = 200;
+  const heightPerTask = 50;
+  const newHeight = baseHeight + heightPerTask;
+  container.style.minHeight = `${newHeight}px`;
 }
 function recibirTexto() {
   console.log(input.value);
@@ -23,7 +34,9 @@ function recibirTexto() {
   tasks.appendChild(task);
   console.log(task);
   if (input.value != "") {
-    console.log("Si hay");
+    adjustContainerHeight();
+
+    console.log("Nueva tarea agregada");
     counter++;
     console.log(counter);
     total.innerHTML = `Total: ${counter}`;
@@ -32,17 +45,43 @@ function recibirTexto() {
     ids.appendChild(id);
     let checkboxes = document.createElement("div");
     checkboxes.className = "checkboxes";
-    container.appendChild(checkboxes);
+    statusBoxes.appendChild(checkboxes);
     let checkboxright = document.createElement("input");
     checkboxright.type = "checkbox";
-    checkboxright.id = "right";
+    checkboxright.className = "right";
+    checkboxright.id = `right${id.innerHTML}`;
     checkboxes.appendChild(checkboxright);
+    checkboxright.addEventListener("change", function () {
+      if (this.checked) {
+        counterResult++;
+        result.innerHTML = `Realizadas: ${counterResult}`;
+      } else {
+        counterResult--;
+        result.innerHTML = `Realizadas: ${counterResult}`;
+      }
+    });
+
     let checkboxwrong = document.createElement("input");
     checkboxwrong.type = "checkbox";
-    checkboxwrong.id = "wrong";
+    checkboxwrong.className = "wrong";
+    checkboxwrong.id = `wrong${id.innerHTML}`;
+
+    checkboxwrong.addEventListener("change", function () {
+      if (this.checked) {
+        setTimeout(() => {
+          task.remove();
+          id.remove();
+          checkboxes.remove();
+          counter--;
+          total.innerHTML = `Total: ${counter}`;
+          console.log("Tarea eliminada");
+        }, 100);
+      }
+    });
+
     checkboxes.appendChild(checkboxwrong);
   } else {
-    console.log("No hay");
+    console.log("No hay nueva tarea");
   }
 }
 verificar.addEventListener("click", () => {
